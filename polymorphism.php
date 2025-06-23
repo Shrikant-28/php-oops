554189fdab675c167d7bf55fbeefdfc7e2c15b65
Shrikant Dharam <59720723+Shrikant-28@users.noreply.github.com>
2025-06-24 00:46:50 +0530

    Create polymorphism.php

---
+++ polymorphism.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+* POLYMORPHISM - 
+* Definition: Polymorphism allows different classes to define methods with the same name but different behaviors.
+*/
+
+class Animal {
+    public function makeSound(){
+        echo "Animal sound\n";
+    }
+}
+
+class Dog extends Animal {
+     public function makeSound(){
+        echo "bhao\n";
+    }
+}
+
+class Cat extends Animal {
+     public function makeSound(){
+        echo "Meow\n";
+    }
+}
+
+function animalSound(Animal $animal){
+    $animal->makeSound();
+}
+
+animalSound(new Dog());    // bhao 
+animalSound(new Cat());    // Meow
