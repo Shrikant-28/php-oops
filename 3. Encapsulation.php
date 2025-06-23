9b187df17ae6fa792a96249da6e06640bca56f95
Shrikant Dharam <59720723+Shrikant-28@users.noreply.github.com>
2025-06-24 00:54:58 +0530

    Update and rename Encapsulation.php to 3. Encapsulation.php

--- 3. Encapsulation.php
+++ 3. Encapsulation.php
@@ -2,9 +2,10 @@
 
 /**
 * Encapsulation - Definition: Encapsulation is the practice of hiding the internal state of an object and requiring all interaction to be performed through an object's methods.
+* Encapsulation protects data using `private` or `protected` access modifiers and exposes it via `public`
 */
 
-class BankAccount {
+class Account {
     private $balance = 0;
 
     public function deposit($amount) {
@@ -18,7 +19,7 @@ class BankAccount {
     }
 }
 
-$account = new BankAccount();
-$account->deposit(100);
-echo $account->getBalance(); // Output: 100
-// $account->balance = 1000; // Error: Cannot access private property
+$account = new Account();
+$account->deposit(500);
+echo $account->getBalance(); // Output: 500
+
