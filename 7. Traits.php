bf1bb35f7218f90066ff53b21573dab49cca9fda
Shrikant Dharam <59720723+Shrikant-28@users.noreply.github.com>
2025-07-02 10:15:00 +0530

    Interview Question Update

--- 7. Traits.php
+++ 7. Traits.php
@@ -16,3 +16,37 @@ class User {
 
 $user = new User();
 $user->log("User created"); // Output: Log: User created
+
+#######################################################################
+
+interface HelloInterface {
+    public function sayHellow();
+}
+
+trait SayHello {
+    public function sayHellow() {
+        echo "Hi";
+    }
+}
+
+class A implements HelloInterface {
+    use SayHello;
+    public function sayHellow() {
+        echo "Hi, I am in A";
+    }
+}
+
+class C implements HelloInterface {
+    use SayHello;
+}
+
+function sayHello(HelloInterface $s){
+    return $s->sayHellow();
+}
+
+$c = new C();
+sayHello($c); // Output: Hi
+
+$a = new A();
+sayHello($a); // Output: Hi, I am in A
+
